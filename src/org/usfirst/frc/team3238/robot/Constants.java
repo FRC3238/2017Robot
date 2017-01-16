ad777b27223f8131114564cc2d92009e07e16500
JeffersonButler <Butlejef000@stu.asd103.org>
2017-01-16 13:46:23 -0800

    Added Constants

---
+++ src/org/usfirst/frc/team3238/robot/Constants.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team3238.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+import java.security.PublicKey;
+
+/**
+ * Created by BUTLEJEF000 on 1/16/2017.
+ */
+public class Constants
+{
+    public class Chassis
+    {
+        public static final int DRIVE_TALON_ID_RIGHT_A = 1;
+        public static final int DRIVE_TALON_ID_RIGHT_B = 2;
+        public static final int DRIVE_TALON_ID_LEFT_A = 3;
+        public static final int DRIVE_TALON_ID_LEFT_B = 4;
+    }
+    
+    public class Robot
+    {
+        public static final int MAIN_JOYSTICK_PORT = 0;
+    }
+}
