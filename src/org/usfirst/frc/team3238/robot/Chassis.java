0592f1fbdfe7a1317d42a4f342f885b3247c5201
Garrett Smith <SMITHGAR001@stu.asd103.org>
2017-01-25 18:36:19 -0800

    previous

--- src/org/usfirst/frc/team3238/robot/Chassis.java
+++ src/org/usfirst/frc/team3238/robot/Chassis.java
@@ -7,32 +7,36 @@ import edu.wpi.first.wpilibj.RobotDrive;
 /**
  * Created by BUTLEJEF000 on 1/16/2017.
  */
-public class Chassis
-{
-    RobotDrive driveTrain;
-    
-//    CANTalon leftTalonA, leftTalonB, rightTalonA, rightTalonB;
-    
+public class Chassis {
+
+    CANTalon leftTalonA, leftTalonB, rightTalonA, rightTalonB;
+
     Joystick joy;
-    
+
     Chassis(CANTalon leftTalonA, CANTalon leftTalonB, CANTalon rightTalonA,
-            CANTalon rightTalonB, Joystick joy)
-    {
-//        this.leftTalonA = leftTalonA;
-//        this.leftTalonB = leftTalonB;
-//        this.rightTalonB = rightTalonB;
-//        this.rightTalonA = rightTalonA;
+            CANTalon rightTalonB, Joystick joy) {
+        this.leftTalonA = leftTalonA;
+        this.leftTalonB = leftTalonB;
+        this.rightTalonB = rightTalonB;
+        this.rightTalonA = rightTalonA;
         this.joy = joy;
-        
-        driveTrain = new RobotDrive(leftTalonA, leftTalonB, rightTalonA, rightTalonB);
+
     }
-    
-    public void run(){
-        driveTrain.arcadeDrive(joy.getY(), joy.getTwist());
+
+    public void run() {
+        arcadeDrive(joy.getY(), joy.getTwist());
     }
-    
-    public void autoRun (double y, double twist){
-        driveTrain.arcadeDrive(y, twist);
+
+    public void autoRun(double y, double twist) {
+        arcadeDrive(y, twist);
+
+    }
+
+    public void arcadeDrive(double forward, double turn) {
+        leftTalonA.set(forward + turn);
+        leftTalonB.set(forward + turn);
+        rightTalonA.set(forward - turn);
+        rightTalonB.set(forward - turn);
     }
 }
 
